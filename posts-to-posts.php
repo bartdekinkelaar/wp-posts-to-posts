534a6ccca698a6277f97ca6ed19a3821b7bbe714
Kiran Prajapati <officeofkiran@gmail.com>
2025-02-19 20:12:36 +0530

    1.7.5

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -2,7 +2,9 @@
 /*
 Plugin Name: Posts 2 Posts
 Description: Create many-to-many relationships between all types of posts.
-Version: 1.7.4
+Version: 1.7.5
+Requires at least: 6.0
+Requires PHP: 5.6
 Author: scribu
 Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/posts-to-posts
@@ -10,7 +12,7 @@ Text Domain: posts-to-posts
 Domain Path: /lang
 */
 
-define( 'P2P_PLUGIN_VERSION', '1.7.4' );
+define( 'P2P_PLUGIN_VERSION', '1.7.5' );
 
 define( 'P2P_TEXTDOMAIN', 'posts-to-posts' );
 
